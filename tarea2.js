8ce1055db1fd0922b6576a3b84acff2b20ff5edc
Diamela <diamelavlioi@gmail.com>
2022-08-30 14:51:56 -0300

    tarea2

---
+++ tarea2.js
@@ -0,0 +1,10 @@
+const miNombre = 'Diamela';
+const miEdad = 40;
+const desarrollador = false;
+const fechaNacimiento = new Date(1981, 11 - 1, 16);
+const Libro = {
+    titulo: 'El poder del ahora',
+    autor: 'Eckhart Tolle',
+    fecha: 2016,
+    url: 'https://www.tematika.com/el-poder-del-ahora-9789502805924.html'
+};
